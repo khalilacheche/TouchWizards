a118b35861381a7c2441f685cce2e17368206b4f
Khalil Acheche <khalil.acheche@epfl.ch>
2023-01-23 21:17:13 +0100

    Delete Assets directory

--- Assets/Scripts/DeathAnim.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DeathAnim : MonoBehaviour {
-	public void OnFinishAnim () {
-		Destroy(gameObject);
-	}
-}
